5b950d79dec5a134d8a4d925cbf55690a3cef3e8
MdMahbub-h <2021339001@student.sust.edu>
2024-11-10 04:36:04 +0600

    Update WelcomeScene.js

--- scenes/WelcomeScene.js
+++ scenes/WelcomeScene.js
@@ -6,48 +6,48 @@ class WelcomeScene extends Phaser.Scene {
     super("WelcomeScene");
   }
   preload() {
-    this.load.image("welcomebg", "/assets/welcomebg.png");
-    this.load.image("progress", "/assets/progress.png");
-    this.load.image("progress2", "/assets/progress2.png");
-    this.load.image("playbtn", "/assets/wooden/Asset 11button.png");
-    this.load.image("soundOn", "/assets/wooden/Asset 7.png");
-    this.load.image("soundOff", "/assets/wooden/Asset 6.png");
-    this.load.image("detailsBox", "/assets/wooden/Asset 12button.png");
-    this.load.audio("startSound", "/assets/startSound.mp3");
-    this.load.image("winbg", "/assets/winbg.png");
-    this.load.image("homebtn", "/assets/wooden/Asset 8.png");
-    this.load.image("replaybtn", "/assets/wooden/Asset 11.png");
-    this.load.image("honeypot", "/assets/honeypot.png");
+    this.load.image("welcomebg", "assets/welcomebg.png");
+    this.load.image("progress", "assets/progress.png");
+    this.load.image("progress2", "assets/progress2.png");
+    this.load.image("playbtn", "assets/wooden/Asset 11button.png");
+    this.load.image("soundOn", "assets/wooden/Asset 7.png");
+    this.load.image("soundOff", "assets/wooden/Asset 6.png");
+    this.load.image("detailsBox", "assets/wooden/Asset 12button.png");
+    this.load.audio("startSound", "assets/startSound.mp3");
+    this.load.image("winbg", "assets/winbg.png");
+    this.load.image("homebtn", "assets/wooden/Asset 8.png");
+    this.load.image("replaybtn", "assets/wooden/Asset 11.png");
+    this.load.image("honeypot", "assets/honeypot.png");
 
-    this.load.image("front", "/assets/depth5.png");
-    this.load.image("bg", "/assets/fullbg.png");
-    this.load.image("icon", "/assets/icon.png");
-    this.load.image("rock", "/assets/rock.png");
-    this.load.spritesheet("stay", "/assets/stay.png", {
+    this.load.image("front", "assets/depth5.png");
+    this.load.image("bg", "assets/fullbg.png");
+    this.load.image("icon", "assets/icon.png");
+    this.load.image("rock", "assets/rock.png");
+    this.load.spritesheet("stay", "assets/stay.png", {
       frameWidth: 426 / 4,
       frameHeight: 200,
     });
-    this.load.spritesheet("run", "/assets/run.png", {
+    this.load.spritesheet("run", "assets/run.png", {
       frameWidth: (563 * 2) / 6,
       frameHeight: 98 * 2,
     });
-    this.load.spritesheet("jump", "/assets/jump.png", {
+    this.load.spritesheet("jump", "assets/jump.png", {
       frameWidth: (686 * 2) / 9,
       frameHeight: 100 * 2,
     });
-    this.load.spritesheet("slip", "/assets/slip.png", {
+    this.load.spritesheet("slip", "assets/slip.png", {
       frameWidth: 1836 / 9,
       frameHeight: 129,
     });
-    this.load.spritesheet("fall", "/assets/fall.png", {
+    this.load.spritesheet("fall", "assets/fall.png", {
       frameWidth: (465 * 2) / 5,
       frameHeight: 90 * 2,
     });
-    this.load.spritesheet("coin", "/assets/coin.png", {
+    this.load.spritesheet("coin", "assets/coin.png", {
       frameWidth: 100,
       frameHeight: 100,
     });
-    this.load.spritesheet("bird", "/assets/bird.png", {
+    this.load.spritesheet("bird", "assets/bird.png", {
       frameWidth: 1200 / 6,
       frameHeight: 188,
     });
